fb1c4190991d84dd79115c047187422183e4afa6
Irenaeus <>
2025-02-27 16:11:00 -0600

    Trying to set up automatically upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="mobaseq",
+    version="0.1.0",
     packages=find_packages(include=['mobaseq', 'mobaseq.*']),
     package_dir={'': '.'},
-    include_package_data=True
+    include_package_data=True,
+    dependency_links=[
+        'git+https://github.com/IrenaeusChan/mobaseq_dev.git@main#egg=mobaseq-0.1.0'
+    ],
+    python_requires='>=3.7'
 )
